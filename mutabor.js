b54cfc4f98e5ce8af4c561c28cd952c9db92319c
eskat0n <muyou.prj@gmail.com>
2012-02-15 17:06:48 +0600

    added support for capturing children elements by selector

--- mutabor.js
+++ mutabor.js
@@ -1,43 +1,56 @@
-HTMLElement.prototype.is = function (selector) {
-    var clone = this.cloneNode(true)
-
-    var outerWrapper = document.createElement('div')
-    var innerWrapper = document.createElement('div')
-
-    innerWrapper.id = 'temp' + new Date().getTime() + Math.round(Math.random() * 1000)
-
-    outerWrapper.appendChild(innerWrapper)
-    innerWrapper.appendChild(clone)
-
-    return !!outerWrapper.querySelector('#' + innerWrapper.id + ' > ' + selector)
-}
-
-window.mutabor = window.mutabor || (function () {
-    var mutabor = {
-        _interceptors: [],
-
-        _registerInterceptor: function (type, handler) {
+window.mutabor = (function () {
+    var _interceptors = [],
+        _registerInterceptor = function (type, handler) {
             window.document.addEventListener(type, handler)
-            this._interceptors.push({
+            _interceptors.push({
                 'type': type,
                 'handler': handler
             })
         },
+        _is = function (element, selector) {
+            var clone = element.cloneNode(true)
 
+            var outerWrapper = document.createElement('div')
+            var innerWrapper = document.createElement('div')
+
+            innerWrapper.id = 'temp' + new Date().getTime() + Math.round(Math.random() * 1000)
+
+            outerWrapper.appendChild(innerWrapper)
+            innerWrapper.appendChild(clone)
+
+            return !!outerWrapper.querySelector('#' + innerWrapper.id + ' > ' + selector)
+        }
+
+    return {
         insert: function (selector, callback) {
             if (!callback) {
                 callback = selector
                 selector = null
             }
 
-            this._registerInterceptor('DOMNodeInserted', function (event) {
+            _registerInterceptor('DOMNodeInserted', function (event) {
                 var target = event.srcElement || event.target
                 var container = event.relatedNode
 
-                if (selector && target.is(selector))
+                var applyCallback = function () {
                     if (callback(target, event) === false)
                         if (container)
                             container.removeChild(target)
+                }
+
+                if (selector) {
+                    var childTarget = target.querySelector(selector)
+
+                    if (_is(target, selector)) {
+                        applyCallback()
+                    } else if (childTarget) {
+                        target = childTarget
+                        applyCallback()
+                    }
+                }
+                else {
+                    applyCallback()
+                }
             })
         },
 
@@ -55,18 +68,16 @@ window.mutabor = window.mutabor || (function () {
 
         on: function (type) {
             var params = Array.prototype.slice.call(arguments, 1)
-            window.mutabor[type + 'Filter'].apply(this, params)
+            this[type].apply(this, params)
         },
 
         reset: function () {
-            this._interceptors.forEach(function (f) {
+            _interceptors.forEach(function (f) {
                 window.document.removeEventListener(f.type, f.handler)
             })
-            this._interceptors = []
+            _interceptors = []
         }
-    }
-
-    return mutabor
+    };
 })()
 
 
