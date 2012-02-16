1029bbfd7bebdfe36adb709d1fe2541c2072ae9f
eskat0n <muyou.prj@gmail.com>
2012-02-16 11:44:37 +0600

    fix

--- mutabor.js
+++ mutabor.js
@@ -1,56 +1,56 @@
 window.mutabor = (function () {
-    var _interceptors = []
+    var _interceptors = [];
 
     var _is = function (element, selector) {
         if (!element.querySelector)
-            return false
+            return false;
 
-        var clone = element.cloneNode(true)
+        var clone = element.cloneNode(true);
 
-        var outerWrapper = document.createElement('div')
-        var innerWrapper = document.createElement('div')
+        var outerWrapper = document.createElement('div');
+        var innerWrapper = document.createElement('div');
 
-        innerWrapper.id = 'temp' + new Date().getTime() + Math.round(Math.random() * 1000)
+        innerWrapper.id = 'temp' + new Date().getTime() + Math.round(Math.random() * 1000);
 
-        outerWrapper.appendChild(innerWrapper)
-        innerWrapper.appendChild(clone)
+        outerWrapper.appendChild(innerWrapper);
+        innerWrapper.appendChild(clone);
 
         return !!outerWrapper.querySelector('#' + innerWrapper.id + ' > ' + selector)
-    }
+    };
 
     var _registerInterceptor = function (type, selector, interceptorInvoker) {
         var eventHandler = function (event) {
-            var target = event.srcElement || event.target
+            var target = event.srcElement || event.target;
 
             if (selector) {
-                var childTarget = taret.querySelector ? target.querySelector(selector) : null
+                var childTarget = target.querySelector ? target.querySelector(selector) : null;
 
                 if (_is(target, selector))
-                    interceptorInvoker(target, event)
+                    interceptorInvoker(target, event);
                 else if (childTarget)
                     interceptorInvoker(childTarget, event)
             }
             else {
                 interceptorInvoker(target, event)
             }
-        }
+        };
 
-        document.addEventListener(type, eventHandler)
+        document.addEventListener(type, eventHandler);
         _interceptors.push({
             'type': type,
             'handler': eventHandler
         })
-    }
+    };
 
     return {
         insert: function (selector, interceptor) {
             if (!interceptor) {
-                interceptor = selector
+                interceptor = selector;
                 selector = null
             }
 
             _registerInterceptor('DOMNodeInserted', selector, function (target, event) {
-                var container = event.relatedNode
+                var container = event.relatedNode;
                 if (interceptor(target, event) === false)
                     if (container)
                         container.removeChild(target)
@@ -59,7 +59,7 @@ window.mutabor = (function () {
 
         remove: function (selector, interceptor) {
             if (!interceptor) {
-                interceptor = selector
+                interceptor = selector;
                 selector = null
             }
 
@@ -68,23 +68,23 @@ window.mutabor = (function () {
             })
         },
 
-        attribute: function (selector, interceptor) {
+        attribute: function () {
             throw 'DOMAttrModified is not supported in WebKit so this interceptor is not implemented'
         },
 
-        text: function (selector, interceptor) {
+        text: function () {
             throw 'DOMCharacterDataModifiedis not supported in WebKit so this interceptor is not implemented'
         },
 
         on: function (type) {
-            var params = Array.prototype.slice.call(arguments, 1)
+            var params = Array.prototype.slice.call(arguments, 1);
             this[type].apply(this, params)
         },
 
         reset: function () {
             _interceptors.forEach(function (f) {
                 document.removeEventListener(f.type, f.handler)
-            })
+            });
             _interceptors = []
         },
 
@@ -92,4 +92,4 @@ window.mutabor = (function () {
             return 'mutabor version 0.1'
         }
     };
-})()
+})();
