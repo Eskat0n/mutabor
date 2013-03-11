1ee566d51188b0c29565bc16983185564345cf7b
Alexander I. Zaytsev <hazzik@gmail.com>
2013-03-12 01:28:06 +1300

    Remove closure from _detectCapabilities

--- mutabor.js
+++ mutabor.js
@@ -71,12 +71,11 @@ window.mutabor = (function () {
         div.style.display = 'none';
 
         for (var i = 0; i < _eventTypes.length; i++) {
-            (function (eventType) {
-                caps[eventType] = false;
-                div.addEventListener(eventType, function () {
-                    caps[eventType] = true;
-                });
-            })(_eventTypes[i]);
+            var eventType = _eventTypes[i];
+            caps[eventType] = false;
+            div.addEventListener(eventType, function (evt) {
+                caps[evt.type] = true;
+            });
         }
 
         var body = document.getElementsByTagName('body')[0];
